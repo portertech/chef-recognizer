fd2cde0654e0e01fdfb69a7fe3aab127f6ac0ba7
Sean Porter <portertech@gmail.com>
2012-08-09 00:48:05 -0700

    [tar] use release tars

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default.recognizer.version = "0.1.6"
+default.recognizer.version = "0.2.0"
 default.recognizer.user = "recognizer"
 default.recognizer.directory = "/etc/recognizer"
 default.recognizer.log.directory = "/var/log/recognizer"
