a05c5470999fed8cb45e30c5a137f991af1f8270
Sean Porter <portertech@gmail.com>
2012-12-11 22:28:04 -0800

    cookbook version bump

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,13 @@ maintainer_email "portertech@gmail.com"
 license          "MIT"
 description      "Installs/Configures Recognizer"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.5"
+version          "0.0.6"
 
 # available @ http://community.opscode.com/cookbooks/java
 depends "java"
 
 %w[
   ubuntu
-  debian
 ].each do |os|
   supports os
 end
